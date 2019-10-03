18d4c7d79bd1b83cf19a8f747751f9f61636fee1
Niraj1920 <neerajkachhwah@gmail.com>
2019-10-03 19:01:08 +0530

    Update scatter Palindrome.cpp

--- EdgeVerve/scatter Palindrome.cpp
+++ EdgeVerve/scatter Palindrome.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #include <string>
+//this is for all possible su
 using namespace std;
 bool isPalindrome(string temp){
     int i=0, j=temp.length()-1;
@@ -10,6 +11,22 @@ bool isPalindrome(string temp){
             i++; j--;
         }
     }
+    return true;
+}
+//this is for scatter palindrome
+bool canbePalindrome(string temp){
+    int a[26] = {0};
+    for(int i=0; i<temp.length(); i++){
+        cout<<temp[i]-97<<" ";
+        a[temp[i]-97]++;
+    }
+    cout<<endl;
+    int count = 0;
+    for(int i=0; i<26; i++){
+        cout<<a[i]<<" ";
+        if(a[i]%2 == 1) count++;
+    }cout<<endl;
+    if(count > 1)   return false;
     return true;
 }
 int main()
@@ -22,7 +39,7 @@ int main()
         for(int j=1; j<=s.length()-i; j++){
             string temp = s.substr(i, j);
             //cout<<temp<<endl;
-            if(isPalindrome(temp)){
+            if(iscanbePalindrome(temp)){
                 cout<<temp<<" "<<++count<<endl;
             } 
         }
